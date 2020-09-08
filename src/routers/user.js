8c2758504f2b679645ce43d081e61e95ec85924d
Charles Chen <chenssuchi09@gmail.com>
2020-09-09 00:57:48 +0800

    Add avatar authentication and avatar delete

--- src/routers/user.js
+++ src/routers/user.js
@@ -84,7 +84,6 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars', // the destination we want to store the file,
     limits: {
         fileSize: 1000000 // 1MB
     },
@@ -97,10 +96,18 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => { // Handling customized express error
     res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 module.exports = router
